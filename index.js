a8839d563694e689a071c52a1bedf8d90ac71d64
evgesha <evgesha@MacBook-Pro-evgesha.local>
2019-02-26 22:06:55 +0300

    small refactoring + resolving bit issues

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 // Here's my data model
-var ViewModel = function(bookmarks) {
+
+var ViewModel = function (bookmarks) {
     this.name = ko.observable();
     this.url = ko.observable();
     this.bookmarks = ko.observableArray(bookmarks);
-
-    this.formIsValid = ko.computed(function() {
-        if(!this.name() || !this.url()){
-            return false;
-        }
-
-        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-        var regex = new RegExp(expression);
-
-        if(!this.url().match(regex)){
-            return false;
-        }
-
-        return true;
-    }, this);
 
     this.saveBookmark = function () {
 
@@ -32,17 +18,33 @@ var ViewModel = function(bookmarks) {
         this.resetForm();
     };
 
-    this.resetForm = function () {
-        this.name('');
-        this.url('');
+    this.deleteBookmark = function (bookmark) {
+        this.bookmarks.remove(bookmark);
+        this.updateStorage();
     };
 
     this.updateStorage = function () {
         localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks()));
     };
 
-    this.deleteBookmark = function (bookmark) {
-        this.bookmarks.remove(bookmark);
+    this.formIsValid = ko.computed(function () {
+        if (!this.name() || !this.url()) {
+            return false;
+        }
+
+        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(expression);
+
+        if (!this.url().match(regex)) {
+            return false;
+        }
+
+        return true;
+    }, this);
+
+    this.resetForm = function () {
+        this.name('');
+        this.url('');
     };
 
     this.addhttp = function (bookmark) {
@@ -52,9 +54,8 @@ var ViewModel = function(bookmarks) {
             correctUrl = `http://${bookmark.url}`;
         }
 
-    return correctUrl;
-}
+        return correctUrl;
+    }
 };
 
 ko.applyBindings(new ViewModel(localStorage.getItem('bookmarks') != "undefined" ? JSON.parse(localStorage.getItem('bookmarks')) : undefined));
-
